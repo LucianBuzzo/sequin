299a4b3121370ce076f8ac634c41f8f89ad238d7
Lucian <lucian.buzzo@gmail.com>
2021-03-06 22:21:49 +0000

    feat(Server): Generate server wallet deterministically
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- src/wallet.cr
+++ src/wallet.cr
@@ -2,12 +2,17 @@ require "secp256k1"
 
 class Wallet
   def initialize()
-    rand = Random::Secure.random_bytes(64)
-    hash = OpenSSL::Digest.new("SHA256").update(rand).final.hexstring
+    rand = Random::Secure.random_bytes(64).to_s
+    puts rand
+    initialize(rand)
+  end
+
+  def initialize(seed : String)
+    hash = OpenSSL::Digest.new("SHA256").update(seed).final.hexstring
 
     # SHA256 will generate a 64 character hash in base 16, which can be converted to a BigInt. The keypair seed has to be a BigInt
-    seed = BigInt.new(hash, 16)
-    @keypair = Secp256k1::Keypair.new seed
+    seed_int = BigInt.new(hash, 16)
+    @keypair = Secp256k1::Keypair.new seed_int
     @keypair.get_secret
   end
 
